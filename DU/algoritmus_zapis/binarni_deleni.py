36c06678e885d17cebfa88203a9eb023d31a4ffd
Svata <svatuskj@arcig.cz>
2024-09-26 22:26:25 +0200

    casomira

---
+++ DU/algoritmus_zapis/binarni_deleni.py
@@ -0,0 +1,26 @@
+import time
+
+def binarni_deleni(seznam: list[int], cislo: int) -> int | None:
+    right = len(seznam)
+    if right == 0:
+        return None
+    left = 0 
+    half = round((right-left)/2)
+
+    while not (right - left == 0 or right - left == 1):
+        half = round((right-left)/2)
+        if seznam[left+half] > cislo:
+            right = half
+        elif seznam[left+half] < cislo:
+            left = half
+        else:
+            return left + half
+
+    return None
+
+start = time.perf_counter_ns()
+
+res = binarni_deleni([1,4, 5, 14, 66, 77, 90], 14)
+
+print('it took: ', (time.perf_counter_ns() - start), 'nanosecons')
+
