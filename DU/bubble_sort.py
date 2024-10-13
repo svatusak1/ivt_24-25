9518d0a4fd7abc6c839545ddd1e055e32bcb17d6
Svata <svatuskj@arcig.cz>
2024-10-13 18:54:05 +0200

    buble first version

---
+++ DU/bubble_sort.py
@@ -0,0 +1,18 @@
+def bubble_sort(seznam: list[int]) -> list[int]:
+    switched = True
+    len_seznam = len(seznam)
+
+    i = 0
+    while switched:
+        switched = False
+        i = 0
+
+        while i < len_seznam-1:
+            if seznam[i] > seznam[i+1]:
+                seznam[i], seznam[i+1] = seznam[i+1], seznam[i]
+                switched = True
+            i += 1
+    return seznam
+
+
+print(bubble_sort([2, 4, 5, 2, 5, 1, 9]))
